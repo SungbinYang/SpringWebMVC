d9d5f08780fedd55e08ce49c839fdb8bd6713398
SungbinYang <ysb05222@naver.com>
2022-01-31 11:57:15 +0900

    리소스 핸들러

--- demo-boot-web/src/main/java/me/sungbin/demobootweb/WebConfig.java
+++ demo-boot-web/src/main/java/me/sungbin/demobootweb/WebConfig.java
@@ -1,8 +1,12 @@
 package me.sungbin.demobootweb;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.CacheControl;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * packageName : me.sungbin.demobootweb
@@ -23,5 +27,12 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new GreetingInterceptor()).order(0);
         registry.addInterceptor(new AnotherInterceptor()).addPathPatterns("/hi").order(-1);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/mobile/**")
+                .addResourceLocations("classpath:/mobile/")
+                .setCacheControl(CacheControl.maxAge(10, TimeUnit.MINUTES));
     }
 }
--- demo-boot-web/src/test/java/me/sungbin/demobootweb/SampleControllerTest.java
+++ demo-boot-web/src/test/java/me/sungbin/demobootweb/SampleControllerTest.java
@@ -1,14 +1,16 @@
 package me.sungbin.demobootweb;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpHeaders;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 /**
  * packageName : me.sungbin.demobootweb
@@ -41,5 +43,22 @@ class SampleControllerTest {
         this.mockMvc.perform(get("/hello").param("id", savedPerson.getId().toString()))
                 .andDo(print())
                 .andExpect(content().string("hello sungbin"));
+    }
+
+    @Test
+    void 정적리소스테스트() throws Exception {
+        this.mockMvc.perform(get("/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("hello index")));
+    }
+
+    @Test
+    void 정적리소스핸들러테스트() throws Exception {
+        this.mockMvc.perform(get("/mobile/index.html"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(Matchers.containsString("Hello Mobile")))
+                .andExpect(header().exists(HttpHeaders.CACHE_CONTROL));
     }
 }
